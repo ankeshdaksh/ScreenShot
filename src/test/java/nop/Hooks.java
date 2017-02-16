34e1245f0467270bbbd24b025b04a1528e1ce680
kailash <kailash>
2017-02-16 23:14:26 +0000

    Screen Shot

---
+++ src/test/java/nop/Hooks.java
@@ -0,0 +1,29 @@
+package nop;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import utils.BrowserFactory;
+
+/**
+ * Created by Acer on 11/12/2016.
+ */
+public class Hooks {
+
+    public Scenario scenario;
+
+    @Before
+    public void setUp() {
+        BrowserFactory.openBrowser();
+    }
+
+    @After
+    public void teaDown(Scenario scenario) {
+        this.scenario = scenario;
+        if (scenario.isFailed()) {
+            BrowserFactory.takeScreenShot(scenario);
+        } else {
+            BrowserFactory.closeBrowser();
+        }
+    }
+}
